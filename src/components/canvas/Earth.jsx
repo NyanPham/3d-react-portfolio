f8a008d41f45c6621e88a38d9f9d9fc79c012c8c
NyanPham <phamthanhnhanussh@gmail.com>
2023-03-04 22:47:24 +0700

    m

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import CanvasLoader from '../loader'
 
 const Earth = () => {
     const earth = useGLTF('./planet/scene.gltf')
@@ -29,7 +28,7 @@ const EarthCanvas = () => {
                 position: [-4, 3, 6],
             }}
         >
-            <Suspense fallback={<CanvasLoader />}>
+            <Suspense fallback={null}>
                 <OrbitControls
                     autoRotate={true}
                     enableZoom={false}
