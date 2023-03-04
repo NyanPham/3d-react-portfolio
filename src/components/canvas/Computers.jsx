7b732e9a8ca5c6b81f10cafcfbd63c15ae006ad0
NyanPham <phamthanhnhanussh@gmail.com>
2023-03-04 22:53:27 +0700

    m

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,7 +2,7 @@ import { Canvas } from '@react-three/fiber'
 import { Suspense, useEffect, useState } from 'react'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
-import CanvasLoader from '../loader'
+import CanvasLoader from '../Loader'
 
 const Computers = ({ isMobile }) => {
     const computer = useGLTF('./desktop_pc/scene.gltf')
